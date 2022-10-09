3583ae22af10272850a2a2cd84343cee24cdd8c4
growwithabhi <115409920+growwithabhi@users.noreply.github.com>
2022-10-09 19:21:03 +0530

    Add files via upload

---
+++ 06_pratice.py
@@ -0,0 +1,75 @@
+# question 1
+# theDist = {
+#         "pankha": "fan",
+#         'dabba': 'box',
+#         'kursi': 'chair'
+# }
+# print("Option are ", theDist.keys())
+# a = input("enter the hindi word\n")
+# # print("The meaning of this word is: ", theDist[a])
+
+
+# #below the line not throw the error if the key is not present in the distionary
+# print("The meaning of this word is: ", theDist.get(a))
+
+# question 2
+# num1 = int(input("Enter number 1\n "))
+# num2 = int(input("Enter number 2\n "))
+# num3 = int(input("Enter number 3\n "))
+# num4 = int(input("Enter number 4\n "))
+# num5 = int(input("Enter number 5\n "))
+# num6 = int(input("Enter number 6\n "))
+# num7 = int(input("Enter number 7\n "))
+# num8 = int(input("Enter number 8\n "))
+
+# s = {num1,num2,num3,num4,num5,num6,num7,num8}
+# print(s)
+
+# Question 3
+# c = {18, "18",18.1}
+# print(c)
+
+# Question 4
+# a = set()
+# a.add(20)
+# a.add(20.0)
+# a.add("20")
+# print(len(a))
+
+# Qustion 5
+# s = {}
+# print(type(s))
+
+# Question 6
+# from cmath import e
+# from this import d
+# from tkinter import W
+
+
+# favlist = {}
+# a = input("Enter your favorite language nikhil\n")
+# b = input("Enter your favorite languag Aman\n")
+# c = input("Enter your favorite language navneet\n")
+# d = input("Enter your favorite language Ravi\n")
+# favlist['nikhil'] = a
+# favlist['Aman'] = b
+# favlist['navneet'] = c
+# favlist['Ravi'] = d
+
+# print(favlist)
+
+# Question 7
+# favlist = {}
+# a = input("Enter your favorite language nikhil\n")
+# b = input("Enter your favorite languag Aman\n")
+# c = input("Enter your favorite language navneet\n")
+# d = input("Enter your favorite language Ravi\n")
+# favlist['nikhil'] = a
+# favlist['Aman'] = b
+# favlist['navneet'] = c
+# favlist['Ravi'] = d
+# print(favlist)
+
+# Question 9 = answer = question is wrong because their is list inside the set
+s = {8, 7, 12, "harry", [1,2]}
+print(s)
